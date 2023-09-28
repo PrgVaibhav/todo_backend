0b2a516710e47b9372ac3e84823c90094ec6987f
prgVaibhav <vaibhav2k1507@gmail.com>
2023-09-28 23:48:39 +0530

    Fixed bugs

--- database/database.js
+++ database/database.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "TODOAPP"
+      dbName: "TODOAPP",
     })
-    .then(() => console.log(`Database is connected and working`))
+    .then((c) =>
+      console.log(`Database is connected and working with ${c.connection.host}`)
+    )
     .catch((err) => console.error(err));
 };
